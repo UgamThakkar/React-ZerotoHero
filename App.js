b1ab0c3c620587d05c40e9530cf8f3f59db16e18
Ugam Thakkar <thakkarugam@gmail.com>
2025-07-29 09:18:54 -0700

    React Basics Ep - 01

---
+++ App.js
@@ -0,0 +1,31 @@
+//nested structures using react.
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement(
+            "h1",
+            {},
+            "I'm a nested H1 Tag"
+        ),
+        React.createElement(
+            "h2", 
+            {}, 
+            "Im a nested H2 tag"
+        )]
+    )
+);
+
+const heading = React.createElement(
+    "h1",
+    {id:"heading"}, //these are attributes
+    "Hello World from React!" //these are children
+);
+
+console.log(heading);  // the heading here is an object - a react element its not a h1 tag yet.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); //render will convert this element into that h1 tag and put in on the DOM.
