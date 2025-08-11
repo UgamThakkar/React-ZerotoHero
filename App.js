f44a8f27c72c82d85e76376f12e07127aa91828b
Ugam Thakkar <thakkarugam@gmail.com>
2025-08-11 08:19:33 -0700

    React Components Ep-02

--- App.js
+++ App.js
@@ -1,33 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-//nested structures using react.
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement(
-            "h1",
-            {},
-            "I'm a nested H1 Tag"
-        ),
-        React.createElement(
-            "h2", 
-            {}, 
-            "Im a nested H2 tag"
-        )]
-    )
-);
 
-const heading = React.createElement(
-    "h1",
-    {id:"heading"}, //these are attributes
-    "Hello World from React!" //these are children
-);
 
-console.log(heading);  // the heading here is an object - a react element its not a h1 tag yet.
+//React Components -  these are just javascript functions that return JSX.
+const Heading = () =>{
+    return <h1>First way to write react Components</h1>
+}
+
+const Heading2 = () =>(
+    <h1>First way to write react Components</h1>
+)
+
+const Heading3 = () => <h1>Third way to write react Components</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent); //render will convert this element into that h1 tag and put in on the DOM.
+root.render(Heading); 
