616297bedfaf197f8aecb119887754b296fb47de
Ugam Thakkar <thakkarugam@gmail.com>
2025-08-11 18:13:16 -0700

    Food Delivery App Starter - hard coded components and props

--- App.js
+++ App.js
@@ -1,18 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-
-//React Components -  these are just javascript functions that return JSX.
-const Heading = () =>{
-    return <h1>First way to write react Components</h1>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/image/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>My Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
-const Heading2 = () =>(
-    <h1>First way to write react Components</h1>
-)
+const RestaurantCard = (props) => {
+    return (
+        <div className="res-card">
+            <img className="res-logo" alt="res-logo" src="https://img.cdn4dd.com/cdn-cgi/image/fit=cover,width=1000,height=300,format=auto,quality=80/https://doordash-static.s3.amazonaws.com/media/store/header/96485b36-053e-459b-a463-894e9e8041d3.png"/>
+            <h3>{props.resname}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.5 Stars</h4>
+            <h4>33 minutes</h4>
+        </div>
+    );
+};
 
-const Heading3 = () => <h1>Third way to write react Components</h1>
+const Body = () =>{
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard resname="Boston Pizza" cuisine="Pizza Fries Beer"/>
+                <RestaurantCard resname="BarBurrito" cuisine="Burritos"/>
+            </div>
+
+        </div>
+    );
+};
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading />); 
+root.render(<AppLayout />); 
