e8de4d4f06ca34317f2afdf389ee266631614221
Ugam Thakkar <thakkarugam@gmail.com>
2025-08-15 11:35:24 -0700

    fix: update Swiggy API calls, add routing with react-router-dom, and implement error page
    - Fixed Swiggy API integration to ensure successful data fetching
    - Integrated react-router-dom for page navigation
    - Added route pages: AboutUs, Contact, etc.
    - Attached global Error page for handling invalid routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body";
+import AboutUs from './components/AboutUs'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
     return (
@@ -12,6 +16,24 @@ const AppLayout = () => {
     );
 };
 
+//creating routing configuration
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout />,
+        errorElement:<Error />
+    },
+    {
+        path:"/about",
+        element: <AboutUs />,
+    },
+    {
+        path:"/contact",
+        element: <Contact />
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />); 
+//we provide the routing configuration to the ROuterProvider component from react router dom which will start using the routing configuration on the page for us.
+root.render(<RouterProvider router={appRouter} />); 
