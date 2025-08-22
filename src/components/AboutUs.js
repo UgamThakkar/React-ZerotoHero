34a64a1b855bcd38cbf485435f674efba6a0567c
Ugam Thakkar <thakkarugam@gmail.com>
2025-08-22 08:14:09 -0700

    Class based components - understanding the react life cycle with examples and how multiple children are rendered

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,14 +1,64 @@
 import React from 'react'
 import User from './User'
 import UserClass from './UserClass'
-const AboutUs = () => {
-  return (
+import React from 'react'
+//starts rendering the cmponent and then noticies that theres a user class component here. So now the instance of this class based component is created and then
+//from the instance the contructor of this class based component is called and then the render method is called.
+class About extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidCatch(){
+    console.log("Parent Componentdidmount");
+  }
+
+  render(){
+    console.log("Parent Render");
+      return (
     <div>
       <h2>AboutUs</h2>
-      <User name = {"Ugam Thakkar for function based components"} location={"Canada"}/>
-      <UserClass name = {"Ugam Thakkar"} location={"Canada"}/>
+      <UserClass name = {"First Child"} location={"Canada"}/>
+      <UserClass name = {"Second Child"} location={"Canada"}/>
+
       </div>
-  )
+    )
+  }
 }
 
-export default AboutUs
+
+/*
+order of exuction - UNDERSTANDING REACT LIFE CYCLE - 2 phases - RENDER PHASE AND COMMIT PHASE: 
+  -Parent Constructor
+  -Parent Render
+    
+    -First Child Constructor
+    -First Child Render
+
+    -Second Child Constructor
+    -Second Child Render
+
+    <DOM Updated - In a single batch>
+    -First Child COmponent DId mount
+    -Second Child Component did mount
+  
+  -Parent Component did mount
+
+  //when we have multiple children - React batches together the render phase of all the children - because its easy right - it only has to find the difference between the virtual DOM's
+  of the children and render those - very quick operation. Step 2 then it batches together the commit phase of all the children together - updates the DOM here and because DOM manipulation is a
+  heavy expensive task it batches all of the updates from all children together and does the updates and then component did mount for all the children are called. 
+  Refer the image -react life cycle diagram
+*/
+
+
+// const AboutUs = () => {
+//   return (
+//     <div>
+//       <h2>AboutUs</h2>
+//       <UserClass name = {"Ugam Thakkar(from class based components)"} location={"Canada"}/>
+//       </div>
+//   )
+// }
+
+export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -8,15 +8,28 @@ class UserClass extends React.Component{
             count2: 2,
         }
 
-        // console.log(props);
+        console.log(this.props.name + "Constructor");
     }
+
+    componentDidMount(){
+        console.log(this.props.name + "component did mount");
+    }
+
+
     render(){
+        console.log(this.props.name + "Render");
+        
         const {name, location} = this.props;
-        const { count, count2 } = this.state;
+        const { count } = this.state;
         return (
             <div className='user-card'>
                 <h2>Count: {count}</h2>
-                <h2>Count2: {count2}</h2>
+                <button onClick={()=>{
+                    //NEVER UPDATE STATE VARIABLES DIRECTLY eg: this.state.count = this.state.count+1 - not allowed this is bad practice.
+                    this.setState({
+                        count: this.state.count + 1, //in class based compnents this is where it will re-render the entire component whenever the count increases.
+                    });
+                }}>Increase Count</button>
                 <h2>{name}</h2>
                 <h3>Location:{location}</h3>
                 <h4>Contact: thakkarugam@</h4>
